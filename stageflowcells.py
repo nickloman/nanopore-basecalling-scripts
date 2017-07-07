628c7f1266781f43f93f20622af6bdd822bc561a
Nick Loman <n.j.loman@bham.ac.uk>
2017-07-07 15:30:48 +0000

    make this work when flowcell id is absent

--- stageflowcells.py
+++ stageflowcells.py
@@ -5,45 +5,70 @@ import os.path
 import sys
 import shutil
 import re
+import argparse
 
-input_dir = sys.argv[1]
-albacore_dir = sys.argv[2]
-process_dir = sys.argv[3]
+def run(args):
+	basecalled_files = set()
 
-basecalled_files = set()
+	for root, dirs, files in os.walk(args.basecalled, topdown=False):
+		for name in files:
+			if name.endswith('.fast5'):
+				basecalled_files.add(name)
 
-for root, dirs, files in os.walk(albacore_dir, topdown=False):
-	for name in files:
-		basecalled_files.add(name)
+	# don't copy already staged files
+	for root, dirs, files in os.walk(args.staging, topdown=False):
+		for name in files:
+			if name.endswith('.fast5'):
+				basecalled_files.add(name)
 
-# don't copy already staged files
-for root, dirs, files in os.walk(process_dir, topdown=False):
-	for name in files:
-		basecalled_files.add(name)
+	for root, dirs, files in os.walk(args.prebasecalled, topdown=False):
+		    for name in files:
+				if name not in basecalled_files:
+					flowcell = ''
+					samplename = ''
+					f = re.search('_2\d{7}_(F.*?)_', name)
+					if f:
+						flowcell = f.group(1)
+					f = re.search('_(sequencing_run|mux_scan)_(.*)_\d+_read', name)
+					if f:
+						samplename = f.group(2)
 
-for root, dirs, files in os.walk(input_dir, topdown=False):
-	    for name in files:
-			if name not in basecalled_files:
-				m = re.search('_(FN.*?)_', name)
-				if not m:
-					continue
-				flowcell = m.group(1)
+					if args.organiseby == 'flowcell' and flowcell:
+						directory_name = flowcell
+					elif args.organiseby == 'sample' and name:
+						directory_name = samplename
+					else:
+						if flowcell and name:
+							directory_name = "%s-%s" % (flowcell, samplename)
+						elif name:
+							directory_name = samplename
+						else:
+							print >>sys.stderr, "Skipping %s" % (name,)
+							continue
 
-				albacore_root = root[len(input_dir):]
-				# move it
-				checkdir = process_dir + '/' + flowcell + '/' + albacore_root
-				if not os.path.exists(checkdir):
-					os.makedirs(checkdir)
-				movefrom = input_dir + '/' + albacore_root + '/' + name
-				moveto = process_dir + '/' + flowcell + '/' + albacore_root + '/' + name
-				print "Copy %s to %s" % (movefrom, moveto)
+					albacore_root = root[len(args.prebasecalled):]
+					# move it
+					checkdir = args.staging + '/' + directory_name + '/' + albacore_root
+					if not os.path.exists(checkdir):
+						os.makedirs(checkdir)
+					movefrom = args.prebasecalled + '/' + albacore_root + '/' + name
+					moveto = args.staging + '/' + directory_name + '/' + albacore_root + '/' + name
+					print "Copy %s to %s" % (movefrom, moveto)
 
-				abspath = os.path.abspath(movefrom)
-				print "Abspath %s" % (abspath,)	
-				os.symlink(abspath, moveto)
+					abspath = os.path.abspath(movefrom)
+					print "Abspath %s" % (abspath,)	
+					os.symlink(abspath, moveto)
 
-#				shutil.copy(movefrom, moveto)
+parser = argparse.ArgumentParser(description='Stage files for processing.')
+parser.add_argument('prebasecalled', 
+                    help='directory containing non-basecalled reads')
+parser.add_argument('basecalled',
+                    help='directory containing basecalled reads')
+parser.add_argument('staging',
+                    help='staging directory')
+parser.add_argument('--organiseby', choices=('wotevs', 'flowcell', 'sample'), default='wotevs',
+                    help='organise reads by specific part of read id')
 
-
-
+args = parser.parse_args()
+run(args)
 
